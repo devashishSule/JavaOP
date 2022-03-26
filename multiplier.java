014f189988b9ed9888d67a99884a4dce154f18c3
Devashish <devashish.sule@gmail.com>
2022-03-26 16:56:05 +0530

    another one

---
+++ multiplier.java
@@ -0,0 +1,17 @@
+class recurrsion{
+    void tableOf(int x){
+        if(x==0 || x==1){
+            System.out.println("Dude, you are dumb.");
+        }
+        for(int i=1; i<=10; i++){
+            System.out.println(x + " * " + i + " = " + x*i);
+        }
+    }
+}
+
+public class multiplier {
+    public static void main(String[] args) {
+        recurrsion math = new recurrsion();
+        math.tableOf(/*number of table you want*/23);
+    }
+}
