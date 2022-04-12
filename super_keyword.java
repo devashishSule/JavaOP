7b45b05db1339f9f482d79e22acc2c3de05d9c13
Devashish <devashish.sule@gmail.com>
2022-04-12 09:13:22 +0530

    another one

--- super_keyword.java
+++ super_keyword.java
@@ -1,9 +1,9 @@
 class naruto{ // parent class
-    String clan = "Karandikar";
+    String clan = "Uzumaki";
 }
 
 class sasuke extends naruto{ // child class
-    String clan = "Sule";
+    String clan = "Uchiha";
     void printClan(){
         System.out.println("Shadow Hokage: Sasuke " + clan); // class sasuke var will be displayed
         System.out.println("Seventh Hokage: Naruto " + super.clan); 
